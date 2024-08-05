bec11834172cdc299af0732734a0cc372ac46eeb
Vadrariu Patric <pativadrariu@gmail.com>
2024-08-05 21:27:57 +0200

    footer

--- presto_vincenzo_patric_rey/resources/views/components/footer.blade.php
+++ presto_vincenzo_patric_rey/resources/views/components/footer.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-</div>
+<div class="card">
+    <h5 class="card-header">Contattaci</h5>
+    <div class="card-body">
+      <h5 class="card-title">Sende:Livorno </h5>
+      <a href="">Presto.It@gmail.com</a>
+      <p>
+        <h3>O Puoi chiamare:</h3>
+        <ul>
+            <li>
+                TEl:345 678 12345
+        
+            </li>
+            <li>
+                Fisso:085 678 12345
+        
+            </li> 
+        </ul>
+    </p>
+    </div>
+  </div>
+  

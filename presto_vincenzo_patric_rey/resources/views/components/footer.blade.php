7d16458f073b83f505fe45172c253458fa25df8b
Vadrariu Patric <pativadrariu@gmail.com>
2024-08-06 18:47:57 +0200

    nuovo footer

--- presto_vincenzo_patric_rey/resources/views/components/footer.blade.php
+++ presto_vincenzo_patric_rey/resources/views/components/footer.blade.php
@@ -5,15 +5,12 @@
       <a href="">Presto.It@gmail.com</a>
       <p>
         <h3>O Puoi chiamare:</h3>
-        <ul>
-            <li>
-                TEl:345 678 12345
-        
-            </li>
-            <li>
-                Fisso:085 678 12345
-        
-            </li> 
+        <ul class="mt-3">
+            <li>Tel:345 678 9030</li>
+            <li>Fiso:089 345 5678</li>
+            <li>Fax:045366275627</li>
+            <li><a href="">presto@gmail.com</a></li>
+            
         </ul>
     </p>
     </div>

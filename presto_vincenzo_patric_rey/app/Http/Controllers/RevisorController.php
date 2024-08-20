0b3053d320c2922dd5af9b1e0fff0c37f1db985c
Vincenzo Jr Colabelli <vincenjr98@icloud.com>
2024-08-20 21:21:02 +0200

    Creazione della Dashboard

--- presto_vincenzo_patric_rey/app/Http/Controllers/RevisorController.php
+++ presto_vincenzo_patric_rey/app/Http/Controllers/RevisorController.php
@@ -9,7 +9,7 @@ class RevisorController extends Controller
 {
     public function index ()
     {
-        $article_to_check =Article::where('is_accepted', null)->first();
-        return view('revisor.index', compact('article_to_chec'));
+        $article_to_check = Article::where('is_accepted', null)->first();
+        return view('revisor.index', compact('article_to_check'));
     }
 }

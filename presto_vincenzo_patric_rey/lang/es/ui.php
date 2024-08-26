0a0d7796e3081da6ab729009f2eb7d8d91171a65
Vadrariu Patric <pativadrariu@gmail.com>
2024-08-26 21:06:14 +0200

    lingua spagnolo

---
+++ presto_vincenzo_patric_rey/lang/es/ui.php
@@ -0,0 +1,32 @@
+<?php 
+return [
+    //homepage
+    'welcome' => 'Bienvenido',
+    'allArticles' => 'Todos los articulos',
+    'nonLogato' => 'QUIERES AÑADIR UN ANUNCIO?',
+    'login' => 'Inicia sesión primero',
+    'register' => '¿Aún no estás registrado? ¡Hazlo ahora!',
+    'buttonRegister' => 'Registro',
+    'noArticles' => 'Aún no hay artículos',
+    'hello'=> 'Hola',
+    'loggato' => 'Aquí puedes crear tu anuncio',
+    'buttonCreate' => 'Crear artículo', 
+    'buttonLogin' => 'Acceso',
+
+    //card 
+     'details'=> 'Detalle',
+     'category' => 'categoría',
+
+     // footer
+     'contacts' => 'Contactos',
+     'revisor1' => 'Quieres convertirte en revisor',
+     'revisor2' => 'Cliccando il bottone sottostante farai richiesta al nostro admin',
+     'revisor3' => 'Convertirse en auditor',
+     //navBar
+     'mySpace' => 'Área personal',
+     'listArticles' => 'Lista de artículos',
+     'logout' => 'Cerrar sesión',
+     'areaRevisor' => 'Área de revisor',
+
+
+    ];

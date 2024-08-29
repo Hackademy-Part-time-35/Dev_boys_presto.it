b5f53c50038091194c92000f4b1e2a6e8a32b42d
Vadrariu Patric <pativadrariu@gmail.com>
2024-08-29 20:03:01 +0200

    componente

---
+++ presto_vincenzo_patric_rey/resources/views/components/pub.blade.php
@@ -0,0 +1,14 @@
+
+<div class="col-3">
+    <div class="video-background mt-5">
+        <video autoplay muted loop>
+            <source src="/video/video.mp4" type="video/mp4">
+            Il tuo browser non supporta i video HTML5.
+        </video>
+        <div class="video-overlay">
+            <h1>Hello, world!</h1>
+            <p>Scopri di più sui nostri servizi e prodotti</p>
+            <a href="/hom/home.html" class="btn btn-primary btn-lg">Scopri di più</a>
+        </div>
+    </div>
+  </div>

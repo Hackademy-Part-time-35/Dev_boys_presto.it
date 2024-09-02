275eb5bad708309ae90b6843d7b159ce4bca245c
Riccardo Costa <riikii1221@gmail.com>
2024-09-02 21:53:57 +0200

    RemoveFace

--- presto_vincenzo_patric_rey/app/Livewire/CreateArticleForm.php
+++ presto_vincenzo_patric_rey/app/Livewire/CreateArticleForm.php
@@ -10,6 +10,7 @@ use Livewire\Component;
 use Livewire\WithFileUploads;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\GoogleVisionLabelImage;
+use App\Jobs\RemoveFaces;
 
 class CreateArticleForm extends Component
 {
@@ -45,10 +46,14 @@ class CreateArticleForm extends Component
             foreach ($this->images as $image){
                 $newFileName ="articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 500, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
-
+                // dispatch(new ResizeImage($newImage->path, 500, 300));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 500, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }

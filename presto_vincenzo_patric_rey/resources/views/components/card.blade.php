97136f22231d3f6794bb07a2b2aca752a3fd5e12
Vadrariu Patric <pativadrariu@gmail.com>
2024-09-05 18:29:37 +0200

    ounded card

--- presto_vincenzo_patric_rey/resources/views/components/card.blade.php
+++ presto_vincenzo_patric_rey/resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 @props(['article'])
 
-<div class="card carte rounded-0 mx-auto card-w text-center mb-4 p-3" style="width: 18rem;">
+<div class="card carte rounded-12 mx-auto card-w text-center mb-4 p-3 " style="width: 18rem;">
     <a href="{{route('article.show', $article)}}"><img src="{{$article->images->isNotEmpty() ? $article->images->first()->getUrl(500, 300): 'https://picsum.photos/500/300'}}"  class="img-fluid bordoFoto" alt="Immagine dell'articolo {{ $article->title }}"></a>
     <div class="card-body p-1">
       <h5 class="card-title mb-3">{{$article->title}}</h5>
@@ -11,7 +11,7 @@
         {{-- <a href="{{route('category.category')}}" class="btn bottone2 truncate-text">{{ $article->category->name }}</a> --}}
         <a href="{{ route('category.category', ['category_id' => $article->category->id]) }}" class="btn bottone2 d-block truncate-text mb-2">{{ ucfirst($article->category->name) }}</a>
         <a href="{{route('article.show', $article)}}" class="btn d-block bottone2 ">{{ __('ui.details')}}</a>
-       
+      
        
       </div>
       <p class="nascondi card-subtitle text-body-secondary fw-light fst-italic text-end pt-3">{{$article->user->name}}</p>
